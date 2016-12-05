5e92efac8c365975d5a2c02494de61b5f14198c8
lanjun <lanjun@tongfangcloud.com>
2016-12-05 15:31:24 +0800

    增加图片

--- WebapiTest/Controllers/ValueController.cs
+++ WebapiTest/Controllers/ValueController.cs
@@ -25,9 +25,42 @@ namespace WebapiTest.Controllers
             }, JsonRequestBehavior.AllowGet);
         }
 
-        private string rootSrc = "http://tnfs.tngou.net/img/";
+        /// <summary>
+        /// 得到文件夹名称
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetDirectoryNames()
+        {
+            string[] ary = Directory.GetDirectories(Server.MapPath("~/Images/"));
+            string[] directorynameAry = ary.Select(item => Path.GetFileName(item)).ToArray();
+            List<DirectoryNameInfo> infos = new List<DirectoryNameInfo>();
+            foreach (string directory in directorynameAry)
+            {
+                string[] tempAry = directory.Split('_');
+                infos.Add(new DirectoryNameInfo() { name = tempAry[1], id = int.Parse(tempAry[0]) });
+            }
+            infos = infos.OrderByDescending(item => item.id).ToList();
+            return Json(infos, JsonRequestBehavior.AllowGet);
+        }
 
-        
+        public JsonResult GetDirectoryImages()
+        {
+            string id = Request.QueryString["id"];
+            string[] ary = Directory.GetDirectories(Server.MapPath("~/Images/"), string.Format("{0}_*", id));
+            if (ary.Length == 0)
+            {
+                return Json(new { result = "id有误" }, JsonRequestBehavior.AllowGet);
+            }
+            string[] files = Directory.GetFiles(ary[0], "*.jpg");
+            files = files.Select(item => "Images/" + Path.GetFileName(ary[0]) + "/" + Path.GetFileName(item)).ToArray();
+            return Json(files, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class DirectoryNameInfo
+    {
+        public string name { set; get; }
+        public int id { set; get; }
     }
 
     public class User
